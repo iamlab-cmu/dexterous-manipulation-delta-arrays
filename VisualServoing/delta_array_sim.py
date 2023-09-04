26d96c9d3badca423b046ea67f31a859aa2d8ab4
nagababa <sbpatil97@gmail.com>
2023-09-04 07:25:24 -0400

    visual servoing added

---
+++ VisualServoing/delta_array_sim.py
@@ -0,0 +1,244 @@
+import numpy as np
+import time
+import pickle as pkl
+from scipy import spatial
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import cv2
+import pandas as pd
+
+from autolab_core import YamlConfig, RigidTransform, PointCloud
+from visualization.visualizer3d import Visualizer3D as vis3d
+
+from isaacgym import gymapi
+from isaacgym_utils.scene import GymScene
+from isaacgym_utils.assets import GymBoxAsset, GymCapsuleAsset
+from isaacgym_utils.math_utils import RigidTransform_to_transform
+from isaacgym_utils.draw import draw_transforms, draw_contacts, draw_camera
+
+import torch
+import networkx as nx
+import utils.nn_helper as helper
+device = torch.device("cuda:0")
+
+class DeltaArraySim:
+    def __init__(self, scene, cfg, obj, obj_name, num_tips = [8,8], run_no = 0):
+        """ Main Vars """
+        self.scene = scene
+        self.cfg = cfg
+        self.run_no = run_no
+        self.num_tips = num_tips
+        self.fingertips = np.zeros((8,8)).tolist()
+        self.cam = 0
+        self.seed = 0
+        self.image_id  = 0
+        self.object = obj
+        self.obj_name = obj_name
+        self.nn_helper = helper.NNHelper()
+        
+        # Introduce delta robot EE in the env
+        for i in range(self.num_tips[0]):
+            for j in range(self.num_tips[1]):
+                self.fingertips[i][j] = (GymCapsuleAsset(scene, **cfg['capsule']['dims'],
+                    shape_props=cfg['capsule']['shape_props'],
+                    rb_props=cfg['capsule']['rb_props'],
+                    asset_options=cfg['capsule']['asset_options']
+                ))
+        """ Data Collection Vars """
+        self.actions = {}
+        self.pre_imgs = {}
+        self.post_imgs = {}
+        self.envs_done = 0
+        self.block_com = np.array(((0.0,0.0),(0.0,0.0)))
+        self.plane_size = 1000*np.array([(0.132-0.025, -0.179-0.055),(0.132+0.025, -0.179+0.055)])
+        cols = ['com_x1', 'com_y1', 'com_x2', 'com_y2'] + [f'robotx_{i}' for i in range(64)] + [f'roboty_{i}' for i in range(64)]
+        self.df = pd.DataFrame(columns=cols)
+        
+        self.time_horizon = 150
+        self.max_vs_steps = 100
+        self.current_vs_steps = 0
+        # self.save_iters = 0
+        # self.num_samples = 100
+        
+        """ Fingertip Vars """
+        self.finger_positions = np.zeros((8,8)).tolist()
+        self.kdtree_positions = np.zeros((64, 2))
+        for i in range(self.num_tips[0]):
+            for j in range(self.num_tips[1]):
+                if j%2==0:
+                    self.finger_positions[i][j] = gymapi.Vec3(j*0.0375, -i*0.043301 - 0.02165, 0.5)
+                    self.kdtree_positions[i*8 + j, :] = (j*0.0375, -i*0.043301 - 0.02165)
+                else:
+                    self.finger_positions[i][j] = gymapi.Vec3(j*0.0375, -i*0.043301, 0.5)
+                    self.kdtree_positions[i*8 + j, :] = (j*0.0375, -i*0.043301)
+        self.neighborhood_fingers = [[] for _ in range(self.scene.n_envs)]
+        self.contact_fingers = [set() for _ in range(self.scene.n_envs)]
+        self.attraction_error = np.zeros((8,8))
+        self.goal = np.zeros((8,8))
+        self.finga_q = gymapi.Quat(0, 0.707, 0, 0.707)
+
+        """ Sim Util Vars """
+        self.attractor_handles = {}
+        self.time_horizon = 107
+
+        """ Visual Servoing Vars """
+        self.current_scene_frame = None
+
+    def set_attractor_handles(self, env_idx):
+        """ Creates an attractor handle for each fingertip """
+        env_ptr = self.scene.env_ptrs[env_idx]
+        self.attractor_handles[env_ptr] = np.zeros((8,8)).tolist()
+        for i in range(self.num_tips[0]):
+            for j in range(self.num_tips[1]):
+                attractor_props = gymapi.AttractorProperties()
+                attractor_props.stiffness = self.cfg['capsule']['attractor_props']['stiffness']
+                attractor_props.damping = self.cfg['capsule']['attractor_props']['damping']
+                attractor_props.axes = gymapi.AXIS_ALL
+
+                attractor_props.rigid_handle = self.scene.gym.get_rigid_handle(env_ptr, f'fingertip_{i}_{j}', 'capsule')
+                self.attractor_handles[env_ptr][i][j] = self.scene.gym.create_rigid_body_attractor(env_ptr, attractor_props)
+
+    def add_asset(self, scene):
+        for i in range(self.num_tips[0]):
+            for j in range(self.num_tips[1]):
+                scene.add_asset(f'fingertip_{i}_{j}', self.fingertips[i][j], gymapi.Transform())
+
+    def set_finger_pose(self, env_idx, pos = "high", all_or_one = "all"):
+        if pos == "high":
+            self.actions[env_idx] = np.zeros((8,8,2))
+        elif all_or_one == "all":
+            self.actions[env_idx] = np.random.uniform(-0.025, 0.025, (8,8,2))
+        else:
+            self.actions[env_idx] = np.zeros((8,8,2))
+
+        for i in range(self.num_tips[0]):
+            for j in range(self.num_tips[1]):
+                if pos=="high":
+                    self.fingertips[i][j].set_rb_transforms(env_idx, f'fingertip_{i}_{j}', [gymapi.Transform(p=self.finger_positions[i][j], r=self.finga_q)])
+                elif (i,j) in self.neighborhood_fingers[env_idx][1]:
+                    self.fingertips[i][j].set_rb_transforms(env_idx, f'fingertip_{i}_{j}', [gymapi.Transform(p=self.finger_positions[i][j] + gymapi.Vec3(0, 0, -0.45), r=self.finga_q)])
+                else:
+                    self.fingertips[i][j].set_rb_transforms(env_idx, f'fingertip_{i}_{j}', [gymapi.Transform(p=self.finger_positions[i][j] + gymapi.Vec3(0, 0, -0.47), r=self.finga_q)])
+                
+
+    def set_block_pose(self, env_idx):
+        # block_p = gymapi.Vec3(np.random.uniform(0,0.313407), np.random.uniform(0,0.2803), self.cfg[self.obj_name]['dims']['sz'] / 2 + 0.002)
+        self.block_com[0] = (0.132, -0.179)
+        block_p = gymapi.Vec3(0.132, -0.179, self.cfg[self.obj_name]['dims']['sz'] / 2 + 0.002)
+        self.object.set_rb_transforms(env_idx, self.obj_name, [gymapi.Transform(p=block_p)])
+
+
+    def reset_finger_pose(self, env_idx):
+        for i in range(self.num_tips[0]):
+            for j in range(self.num_tips[1]):
+                self.fingertips[i][j].set_rb_transforms(env_idx, f'fingertip_{i}_{j}', [gymapi.Transform(p=self.finger_positions[i][j], r=self.finga_q)])
+
+    def get_scene_image(self, env_idx):
+        self.scene.render_cameras()
+        frames = self.cam.frames(env_idx, self.cam_name)
+        self.current_scene_frame = frames
+            
+    def save_cam_images(self, env_idx):
+        # plt.imsave(f'./data/pre_manip_data/image_{self.image_id}_{self.run_no}.png', self.pre_imgs[env_idx].data, cmap = 'gray')
+        final_trans = self.object.get_rb_transforms(env_idx, self.obj_name)[0]
+        self.block_com[1] = (final_trans.p.x, final_trans.p.y)
+        robot_actions = list(self.actions[env_idx][:,:,0].flatten()) + list(self.actions[env_idx][:,:,1].flatten())
+        # print(list(self.block_com.flatten()))
+        # print(robot_actions)
+        self.df.loc[len(self.df)] = list(self.block_com.flatten()) + robot_actions
+        plt.imsave(f'./data/post_manip_data/image_{self.image_id}_{self.run_no}.png', self.pre_imgs[env_idx], cmap = 'gray')
+    
+    def get_nearest_fingertips(self, env_idx, name = 'rope'):
+        # if env_idx == 0:
+        img = self.current_scene_frame['color'].data.astype(np.uint8)
+        plt.imshow(img)
+        plt.show()
+        seg_map = self.current_scene_frame['seg'].data.astype(np.uint8)
+        boundary = cv2.Canny(seg_map,40,200)
+        
+        boundary_pts = np.array(np.where(img==255)).T
+        # self.block_com[0] = np.mean(boundary_pts, axis=0)
+        # plt.scatter(boundary_pts[:,1], boundary_pts[:,0])
+        # plt.show()
+        min_x, min_y = np.min(boundary_pts, axis=0)
+        max_x, max_y = np.max(boundary_pts, axis=0)
+        print(min_x, min_y, max_x, max_y)
+
+        boundary_pts[:,0] = (boundary_pts[:,0] - min_x)/(max_x-min_x)*(self.plane_size[1][0]-self.plane_size[0][0])+self.plane_size[0][0]
+        boundary_pts[:,1] = (boundary_pts[:,1] - min_y)/(max_y-min_y)*(self.plane_size[1][1]-self.plane_size[0][1])+self.plane_size[0][1]
+
+        idxs, neg_idxs, DG, pos = self.nn_helper.get_nn_robots(boundary_pts, num_clusters=40)
+        for idx in idxs:
+            pass
+
+        return None, neg_idxs
+
+    def get_attraction_error(self, idxs):
+        for i,j in enumerate(idxs):
+            final_trans = self.object.get_rb_transforms(env_idx, f'fingertip_{i}_{j}')[0]
+            self.attraction_error[i,j] = np.linalg.norm(np.array((final_trans.p.x, final_trans.p.y, final_trans.p.z)) - goal[i,j])
+        return self.attraction_error
+
+    def visual_servoing(self, scene, env_idx, t_step, _):
+        t_step = t_step % self.time_horizon
+        env_ptr = self.scene.env_ptrs[env_idx]
+        if t_step == 0:
+            self.current_vs_steps += 1
+            # Set all fingers up 
+            self.get_scene_image(env_idx)
+            self.set_finger_pose(env_idx, "high", all_or_one="all")
+            # Get current image of obj
+            self.get_nearest_fingertips(env_idx)
+            # Update nn dict
+            pass
+
+
+
+
+
+
+
+
+
+
+    # def data_collection_expt2(self, scene, env_idx, t_step, _):
+    #     if self.save_iters < 64*self.num_samples:
+    #         data_coll_idx = self.save_iters//self.num_samples
+    #         dci_i, dci_j = data_coll_idx//8, data_coll_idx%8
+    #         t_step = t_step % self.time_horizon
+    #         env_ptr = self.scene.env_ptrs[env_idx]
+    #         if t_step == 0:
+    #             self.view_cam_img(env_idx)
+    #             self.set_finger_pose(env_idx, "high", all_or_one="one")
+    #             self.set_block_pose(env_idx)
+    #         elif t_step == 5:
+    #             self.neighborhood_fingers[env_idx] = self.get_nearest_fingertips(env_idx, name = self.obj_name)
+    #             self.set_finger_pose(env_idx, "low", all_or_one="one")
+    #         elif t_step == 6:
+    #             for i in range(self.num_tips[0]):
+    #                 for j in range(self.num_tips[1]):
+    #                     # Robots above the object shouldn't go all the way down. 
+    #                     if (i,j) in self.neighborhood_fingers[env_idx][1]:
+    #                         self.scene.gym.set_attractor_target(env_ptr, self.attractor_handles[env_ptr][i][j], gymapi.Transform(p=self.finger_positions[i][j] + gymapi.Vec3(0, 0, - 0.45), r=self.finga_q)) 
+    #                     else:
+    #                         if (i==dci_i) and (j==dci_j):
+    #                             # print(self.actions[env_idx])
+    #                             self.actions[env_idx][i][j] = np.random.uniform(-0.025, 0.025, 2)
+    #                             x_move, y_move = self.actions[env_idx][i][j]
+    #                             self.scene.gym.set_attractor_target(env_ptr, self.attractor_handles[env_ptr][i][j], gymapi.Transform(p=self.finger_positions[i][j] + gymapi.Vec3(x_move, y_move, - 0.47), r=self.finga_q))
+    #                         else:
+    #                             self.scene.gym.set_attractor_target(env_ptr, self.attractor_handles[env_ptr][i][j], gymapi.Transform(p=self.finger_positions[i][j] + gymapi.Vec3(0, 0, - 0.45), r=self.finga_q)) 
+    #         elif 6 <= t_step < self.time_horizon - 3:
+    #             pass
+    #         elif t_step == self.time_horizon - 3:
+    #             pass
+    #         elif t_step == self.time_horizon - 1:
+    #             self.reset_finger_pose(env_idx)
+    #             self.view_cam_img(env_idx)
+    #             self.save_cam_images(env_idx)
+    #             self.save_iters += 1
+    #             if self.save_iters%10 == 0:
+    #                 print("HAKUNA")
+    #                 self.df.to_csv(f'./data/post_manip_data/data_{self.image_id}_{self.run_no}.csv', index=False)
+    #         else:
+    #             pass
--- VisualServoing/main.py
+++ VisualServoing/main.py
@@ -2,7 +2,6 @@ import numpy as np
 import os
 import time
 from autolab_core import YamlConfig, RigidTransform
-import Delta_Array_Sim
 from isaacgym import gymapi
 from isaacgym_utils.scene import GymScene
 from isaacgym_utils.assets import GymBoxAsset, GymCapsuleAsset, GymURDFAsset
@@ -11,24 +10,16 @@ from isaacgym_utils.math_utils import RigidTransform_to_transform
 from isaacgym_utils.draw import draw_transforms, draw_contacts, draw_camera
 import torch
 
+import delta_array_sim
+
 class DeltaArrayEnvironment():
     def __init__(self, yaml_path, run_no):
         gym = gymapi.acquire_gym()
         self.run_no = run_no
         self.cfg = YamlConfig(yaml_path)
         self.scene = GymScene(self.cfg['scene'])
-        # if not os.path.exists('./data/manip_data'):   
-        #     os.makedirs('./data/manip_data')
-        #     os.makedirs('./data/pre_manip_data')
-        #     os.makedirs('./data/post_manip_data')
-        #     os.makedirs('./data/graph_manip_data')
-        #     os.makedirs('./data/embed_manip_data')
-        # if not os.path.exists('./data_smol/manip_data'):   
-        #     os.makedirs('./data_smol/manip_data')
-        #     os.makedirs('./data_smol/pre_manip_data')
-        #     os.makedirs('./data_smol/post_manip_data')
-        #     os.makedirs('./data_smol/graph_manip_data')
-        #     os.makedirs('./data_smol/embed_manip_data')
+        if not os.path.exists('./data/manip_data'):   
+            os.makedirs('./data/manip_data')
 
         self.obj_name = "block"
         self.object = GymBoxAsset(self.scene, **self.cfg['block']['dims'], 
@@ -36,7 +27,7 @@ class DeltaArrayEnvironment():
                             rb_props=self.cfg['block']['rb_props'],
                             asset_options=self.cfg['block']['asset_options']
                             )
-        self.fingers = Delta_Array_Sim.DeltaArraySim(self.scene, self.cfg, obj = self.object, obj_name = self.obj_name, num_tips = [8,8], run_no=self.run_no)
+        self.fingers = delta_array_sim.DeltaArraySim(self.scene, self.cfg, obj = self.object, obj_name = self.obj_name, num_tips = [8,8], run_no=self.run_no)
 
         self.cam = GymCamera(self.scene, cam_props = self.cfg['camera'])
         # print(RigidTransform.x_axis_rotation(np.deg2rad(180)))
@@ -56,7 +47,7 @@ class DeltaArrayEnvironment():
         self.setup_objects()
 
     def run(self):
-        self.scene.run(policy=self.fingers.data_collection_expt1)
+        self.scene.run(policy=self.fingers.visual_servoing)
         # self.scene.run(policy=self.fingers.generate_trajectory)
         # self.scene.run(policy=self.fingers.execute_trajectory)
 
